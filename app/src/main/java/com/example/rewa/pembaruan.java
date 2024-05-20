5007f2a9c7105c37a418e7706167dfa3aaf85f3c
fahryanRizky <yaaanchannel@gmail.com>
2024-05-20 20:36:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rewa/pembaruan.java
@@ -0,0 +1,14 @@
+package com.example.rewa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pembaruan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembaruan);
+    }
+}
